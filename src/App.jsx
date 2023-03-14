af738b34797dce1c0bfdb29486cfeeea5be9d65a
Dylan D <dore.dylan31@gmail.com>
2023-03-14 13:31:50 +0100

    reorganize routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { User } from "./pages/User/User";
 import { Footer } from "./components/Footer/Footer";
 import { Navbar } from "./components/Navbar/Navbar";
 import { LoginForm } from "./components/LoginForm/LoginForm";
-import { Cart } from "./components/Cart/Cart";
+import { Cart } from "./pages/Cart/Cart";
+import { NewSession } from "./pages/NewSession/NewSession";
 
 export const App = () => {
   return (
@@ -16,9 +17,10 @@ export const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/*" element={<NotFound />} />
-          <Route path="/User" element={<User />} />
-          <Route path="/LoginForm" element={<LoginForm />} />
-          <Route path="/Cart" element={<Cart />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/loginform" element={<LoginForm />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="api/sessions/create" element={<NewSession />} />
         </Routes>
       </main>
       <Footer />
