4020efb8876a5bfb800f72de16688bea0e5395d3
Dylan D <dore.dylan31@gmail.com>
2023-03-19 03:02:46 +0100

    change links and css nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,28 +2,19 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <div>
-        <ul className="flex flex-row justify-center space-x-5">
-            <li>
-              <Link to="/"> Home </Link>
-            </li>
-            <li>
-              <Link to="/user"> UserPage </Link>
-            </li>
-            <li>
-              <Link to="/Shopping">Chercher des produits</Link>
-            </li>
-            <li>
-              <Link to="Cart"> Panier </Link>
-            </li>
-            <li>
-              <Link to="/LoginForm">Se Connecter</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <nav className="sticky top-0 bg-white border-b border-slate-200">
+      <div className="px-6 sm:px-8 lg:px-16">
+        <div className="flex items-center justify-between h-16 -mb-px">
+            <div className="flex items-center space-x-3">
+              <Link to="/">Accueil</Link>
+            </div>
+            <div className="flex items-center space-x-5">
+              <Link to="/shopping">Produits</Link>
+              <Link to="/cart">Panier</Link>
+              <Link to="/connexion" className="px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-700 rounded-full sm:mx-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Se Connecter</Link>
+            </div>
+          </div>
+      </div>
+    </nav>
   );
 };
