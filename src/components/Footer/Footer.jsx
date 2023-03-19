76a9432fce7fa3a991d922d7f92a823fc74c3667
Dylan D <dore.dylan31@gmail.com>
2023-03-19 17:25:53 +0100

    css footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,7 @@
 export const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer className="px-6 sm:px-8 lg:px-16">
         <p>© 2023 Mon entreprise. Tous droits réservés.</p>
-      </div>
     </footer>
   );
 };
