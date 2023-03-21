ddeea8269fa0cd17937626595ffea23f622b3dde
GillianLEVERT <gillian.levert@gmail.com>
2023-03-21 15:20:51 +0100

    Logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { isAuthenticatedAtom } from "../../store/atom";
 import { useAtom } from "jotai";
+import { Logout } from "../Logout/Logout";
 
 export const Navbar = () => {
   const [isAuthenticated] = useAtom(isAuthenticatedAtom);
@@ -19,10 +20,10 @@ export const Navbar = () => {
               <>
                 <Link to="/cart">Panier</Link>
                 <Link
-                  to="/logout"
+                  to="/"
                   className="px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-700 rounded-full sm:mx-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
                 >
-                  Se Déconnecter
+                  <Logout/>
                 </Link>
               </>
             )}
