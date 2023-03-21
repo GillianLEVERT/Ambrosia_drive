4fa4de41cbf8398e9f93051bbd4d6f7e4166bdd3
Dylan D <dore.dylan31@gmail.com>
2023-03-21 03:39:42 +0100

    AddToCart Button quantity link with backend cart_item quantity

--- src/pages/Shopping/Shopping.jsx
+++ src/pages/Shopping/Shopping.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
-import { useAtomValue } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import { searchResultAtom } from "../../store/atom";
+import { cartItemsAtom } from "../../store/atom";
 import { CardProduct } from "../../components/CardProduct/CardProduct";
+import Cookies from "js-cookie";
 
 export const Shopping = () => {
   const searchResult = useAtomValue(searchResultAtom);
+  const setCartItemAtom = useSetAtom(cartItemsAtom);
+
+  useEffect(() => {
+    fetch("https://ambrosiaserver.fly.dev/cart_items", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Cookies.get("token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCartItemAtom(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
       <SearchBar />
--- src/store/atom.jsx
+++ src/store/atom.jsx
@@ -1,3 +1,5 @@
 import { atom } from "jotai";
 
 export const searchResultAtom = atom(null);
+
+export const cartItemsAtom = atom(null);
